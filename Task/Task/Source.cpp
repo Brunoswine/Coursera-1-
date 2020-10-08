633ef617f6c1b402aabe187dd95aebb1115e4706
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-09 02:05:04 +0300

    NOD2

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -2,53 +2,28 @@
 
 using namespace std;
 
-int NOD(int a, int b) {
-	int nod = 1;
-	int buf;
-	if (a == 0)
-		return b;
-	if (b == 0)
-		return a;
-	if (a == b)
-		return a;
-	if (a == 1 || b == 1)
-		return 1;
-	while (a != 0 && b != 0) {
-		if (a % 2 == 0 && b % 2 == 0) {
-			nod *= 2;
-			a /= 2;
-			b /= 2;
-			continue;
-		}
-		if (a % 2 == 0 && b % 2 != 0) {
-			a /= 2;
-			continue;
-		}
-		if (a % 2 != 0 && b % 2 == 0) {
-			b /= 2;
-			continue;
-		}
-		if (a > b) {
-			buf = a;
-			a = b;
-			b = buf;
-		}
-		buf = a;
-		a = (b - a) / 2;
-		b = buf;
-	}
-	if (a == 0)
-		return nod * b;
-	else
-		return nod * a;
-}
-
-
-int main()
-{
+int main() {
 	int a, b;
 	cin >> a >> b;
-	cout << NOD(a, b);
+
+	// пока оба числа положительны, будем их уменьшать, не меняя их НОД
+	while (a > 0 && b > 0) {
+
+		// возьмём большее из чисел и заменим его остатком от деления на второе
+		// действительно, если a и b делятся на x, то a - b и b делятся на x
+		// тогда и a % b и b делятся на x, так что можно a заменить на a % b
+		if (a > b) {
+			a %= b;
+		}
+		else {
+			b %= a;
+		}
+
+	}
+
+	// если одно из чисел оказалось равно нулю, значит, на последней итерации
+	// большее число разделилось на меньшее
+	cout << a + b;
 	system("pause");
 	return 0;
 }
