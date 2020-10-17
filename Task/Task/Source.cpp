03a353beabbca564593dfb128cb08301c579c27c
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-17 23:13:02 +0300

    Palindrom

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int Factorial(int N) {
-	int sum = 1;
-	if (N <= 0){
-		return 1; 
+bool IsPalindrom(string str){
+	string strReverse=str;
+	int size = str.size() - 1;
+	if (str.size() == 0)return true;
+	for (int i = 0; i <= size;i++) {
+		strReverse[size - i] = str[i];
 	}
-
-
-	for (int i = 1; i <= N; i++) {
-		sum*= i;
+	if (strReverse == str) {
+		return true;
 	}
-	return sum;
+	else return false;
 }
 
-int FactorialRecurs(int x) {
-	if (x <= 1) {
-		return 1;
+bool IsPalindromCoursera(string s) {
+	// Замечание: более правильным было бы использовать здесь тип size_t вместо int
+	// О причинах Вы узнаете на Жёлтом поясе
+	for (int i = 0; i < s.size() / 2; ++i) {
+		if (s[i] != s[s.size() - i - 1]) {
+			return false;
+		}
 	}
-	else {
-		return x * Factorial(x - 1);  // вычисляем факториал от x-1 и умножаем на x
-	}
+	return true;
 }
-
 
 int main() {
-	int a;
-	cin >> a;
-	cout << Factorial(a);
+	string str;
+	cin >> str;
+	cout << IsPalindrom(str);
 	system("pause");
 	return 0;
 }
