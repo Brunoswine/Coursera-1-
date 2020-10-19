93b0151db407500183f04e7d868c2dfb34c5713b
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-19 23:56:35 +0300

    Queue

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -3,31 +3,51 @@
 #include <vector>
 using namespace std;
 
-//Разворот последовательности - 2, Неделя 2
+//Очередь, Неделя 2
 
 
 
 int main() {
-	int n, avrg;
+	int N;
+	bool f;
 	int sum = 0;
-	cin >> n;
-	vector<int> vect(n);
-	for (int &c : vect) {
-		cin >> c;
-		sum += c;
-	}
-	avrg = sum / n;
-	vector<int> itog;
-	for (int i = 0; i < vect.size();i++) {
-		
-		if (vect[i] > avrg) {
-			itog.push_back(i);
+	cin >> N;
+	int command;
+	string strCommand;
+	vector<int> queue;
+	for (int J = 0; J < N; J++) {
+		f = false;
+		cin >> strCommand;
+		if (strCommand == "WORRY_COUNT") {
+			for (int n : queue) {
+				if (n == 1) sum++;
+			}
+			cout << sum << endl;
+			sum = 0;
+			f = true;
+		}
+		if(!f) cin >> command;
+		if (strCommand == "WORRY") {
+			queue[command] = 1;
+		}
+		if (strCommand == "QUIET") {
+			queue[command] = 0;
+		}
+		if (strCommand == "COME") {
+			if (command > 0) {
+				for (int i = 0; i < command; i++) {
+					queue.push_back(0);
+				}
+			}
+			if (command < 0) {
+				for (int i = 0; i < (command*-1); i++) {
+					queue.pop_back();
+				}
+			}
 		}
 	}
-	cout << itog.size() << endl;;
-	for (int c : itog) {
-		cout << c << " ";
-	}
-	system("pause");
+
+	
+	//system("pause");
 	return 0;
 }
