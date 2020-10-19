0b343087473e5be04d7bbe89351150f1bdce5bee
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-20 02:44:48 +0300

    Spisok del

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-//Очередь, Неделя 2
+//Список дел,наконец сделал ааааа Неделя 2
 
 
 
@@ -13,41 +13,67 @@ int main() {
 	int sum = 0;
 	cin >> N;
 	int command;
+	int k;
+	int current_month = 0;
+	string s;
 	string strCommand;
-	vector<int> queue;
-	for (int J = 0; J < N; J++) {
+	vector<int> day_in_month = {32,29,32,31,32,31,32,32,31,32,31,32};
+	vector<vector<string>> LIST(32);
+	for (int i = 0; i < N; i++) {
 		f = false;
 		cin >> strCommand;
-		if (strCommand == "WORRY_COUNT") {
-			for (int n : queue) {
-				if (n == 1) sum++;
+		if (strCommand == "ADD") {
+			cin >> k >> s;
+			LIST[k].push_back(s);
+		}
+		if (strCommand == "DUMP") {
+			cin >> k;
+			cout << LIST[k].size()<<" ";
+			for (int i = 0; i < LIST[k].size();i++) {
+				cout << LIST[k][i]<<" ";
 			}
-			cout << sum << endl;
-			sum = 0;
+			cout << endl;
+		}
+
+		if (strCommand == "Quit") {
 			f = true;
 		}
-		if(!f) cin >> command;
-		if (strCommand == "WORRY") {
-			queue[command] = 1;
-		}
-		if (strCommand == "QUIET") {
-			queue[command] = 0;
-		}
-		if (strCommand == "COME") {
-			if (command > 0) {
-				for (int i = 0; i < command; i++) {
-					queue.push_back(0);
-				}
+		if (f) break;
+		
+		
+		if (strCommand == "NEXT") {
+			current_month++;
+			int day_in_this;
+			int day_in_pred;
+			if (current_month > 11) { current_month = 0; 
+			day_in_pred = 32;
 			}
-			if (command < 0) {
-				for (int i = 0; i < (command*-1); i++) {
-					queue.pop_back();
+			else { day_in_pred = day_in_month[current_month - 1]; }
+			 day_in_this = day_in_month[current_month];
+		 
+			vector<vector<string>> tmp = LIST;
+			LIST.resize(day_in_this);
+			if (day_in_this < day_in_pred) { //если в этом месяце дней меньше, чем в предыдущем
+				int raznica = day_in_pred - day_in_this;
+				for (int i = 0; i < raznica; i++) {
+					/*vector<string> temp; // создадим одинарный вектор того дня, что нужно запушить в последний день и заполним его словами
+					for (int j = 0; j < tmp[day_in_pred - i-1].size(); j++) {
+						temp.push_back(tmp[day_in_pred - i-1][j]);
+					}*/
+					for (string c : tmp[day_in_pred - i - 1]) { // 
+						LIST[day_in_this-1].push_back(c);
+					}
 				}
-			}
-		}
-	}
+			} 
 
+		}
+
+	}
 	
+	
+
+
+
 	//system("pause");
 	return 0;
 }
