394f9239325225969d279b44a6e113fce75386e7
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-19 22:16:29 +0300

    Average Temperature

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -7,52 +7,26 @@ using namespace std;
 
 
 
-void Reverse(vector<int>& vect) {
-	vector<int> temp = vect;
-	vect.clear();
-	for (int x : temp) { // очищаем вектор
-		vect.insert(vect.begin(),x); //вставляем в начало вектора число // первый параметр куда вставляем, второй - что имеено вставляем
-	}
-}
-
-void ReverseCoursera(vector<int>& v) {
-	int n = v.size();  // для удобства сохраним размер вектора в переменную n
-	for (int i = 0; i < n / 2; ++i) {
-		// элемент с индексом i симметричен элементу с индексом n - 1 - i
-		// поменяем их местами с помощью временной переменной tmp
-		int tmp = v[i];
-		v[i] = v[n - 1 - i];
-		v[n - 1 - i] = tmp;
-	}
-}
-
-vector<int> Reversed(const vector<int>& vect) {
-	vector<int> temp;
-	for (int x : vect) {
-		temp.insert(temp.begin(), x); //вставляем в начало вектора число // первый параметр куда вставляем, второй - что имеено вставляем
-	}
-	return temp;
-}
-
-vector<int> ReversedCoursera(const vector<int>& input)
-{
-	vector<int> result;
-	for (int i = input.size() - 1; i >= 0; --i) {
-		result.push_back(input[i]);
-	}
-	return result;
-}
-
-
 int main() {
-	vector<int> vect = {5,4,3};
-	cout << "before"<<endl;
-	for (auto c : vect) {
-		cout << c <<", ";
+	int n, avrg;
+	int sum = 0;
+	cin >> n;
+	vector<int> vect(n);
+	for (int &c : vect) {
+		cin >> c;
+		sum += c;
 	}
-	cout << "after " << endl;
-	for (auto c : Reversed(vect)) {
-		cout << c << ", ";
+	avrg = sum / n;
+	vector<int> itog;
+	for (int i = 0; i < vect.size();i++) {
+		
+		if (vect[i] > avrg) {
+			itog.push_back(i);
+		}
+	}
+	cout << itog.size() << endl;;
+	for (int c : itog) {
+		cout << c << " ";
 	}
 	system("pause");
 	return 0;
