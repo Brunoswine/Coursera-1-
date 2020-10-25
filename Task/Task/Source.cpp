f390f63947d8f65700e0eabf5a1c983c9a6e70d8
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-25 23:56:07 +0300

    CapitalMap

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -4,33 +4,96 @@
 #include <map>
 using namespace std;
 
-//Анаграммы, Неделя 2
+//Справочник столиц, Неделя 2
 
-map<char, int> BuildCharCounters(const string& s1){  // функция которая создает словарь символов(сколько раз символ встречается) для строки 
-	map<char, int> m;
-	for (char word : s1) {  //Обращение к несушествующему ключу создаст его со значением 0;
-		m[word]++;
+
+bool countryExists(const map<string, string>& m, const string& country) {  //True, если такая страна существует, ссылки, чтобы не копировать
+	bool f = false;
+	for (auto &x : m) {
+		if (x.first == country)
+			f = true;
 	}
-	return m;
+	return f;
 }
+
+//Вместо countryExists можно использовать CapMap.count(country) == 0 / CapMap.count(country) == 1
+
 
 int main() {
 	int N;
 	cin >> N;
-	string s1, s2;
-	map<char, int> count1;
-	map<char, int> count2;
 
+
+	string command,country,new_capital;
+	bool f = false;
+	map<string, string> CapMap;
 	for (int i = 0; i < N; i++) {
-		cin >> s1 >> s2;
-		count1 = BuildCharCounters(s1);
-		count2 = BuildCharCounters(s2);
-		if (count1 == count2) {
-			cout << "YES" << endl;
-		} else 
-			cout << "NO" << endl;
+		f = false;								//Сброс флага 
+		cin >> command;
+		if (command == "CHANGE_CAPITAL") {
+			cin >> country;
+			cin >> new_capital;
+
+			f = countryExists(CapMap,country);// если такая страна есть, то нужно поменять, а не добавить. хотя это одно и тоже, хех
+	
+
+			if (f) {
+				const string& old_capital = CapMap[country]; //опять же, не копирование, а создание ссылки на столицу, хотя это почти не ускорит.
+				if (new_capital == old_capital) { // если нечего менять
+					cout << "Country " << country << " hasn't changed its capital" << endl;
+				}
+				else {
+					cout << "Country " << country << " has changed its capital from " << old_capital << " to " << new_capital << endl;
+					CapMap[country] = new_capital;
+				}
+
+			}
+
+			if (!f) {
+				CapMap[country] = new_capital;
+				cout << "Introduce new country " << country << " with capital " << new_capital << endl;
+			}
+
+		}
+
+		if (command == "RENAME") {
+			string old_country_name, new_country_name;
+			cin >> old_country_name >> new_country_name;
+			if ((old_country_name == new_country_name) || (!countryExists(CapMap,old_country_name)) || (countryExists(CapMap, new_country_name))) { // Incorrect rename, skip, если новое название страны совпадает со старым, страна old_country_name не существует или страна new_country_name уже существует
+				cout << "Incorrect rename, skip" << endl;
+			}
+			else{
+				cout << "Country "<< old_country_name<<" with capital " << CapMap[old_country_name]<<" has been renamed to "<< new_country_name << endl;
+				string temp = CapMap[old_country_name];
+				CapMap.erase(old_country_name);
+				CapMap[new_country_name] = temp;
+			}
+
+		}
+		if (command == "ABOUT") {
+			cin >> country;
+			if (!countryExists(CapMap, country)) {
+				cout << "Country " << country << " doesn't exist" << endl;
+			}
+			else { 
+				cout << "Country " << country << " has capital " << CapMap[country] << endl;
+			}
+		}
+		
+
+		if (command == "DUMP") {
+			if (CapMap.size() == 0) {
+				cout<<"There are no countries in the world"<<endl;
+			}
+			for (const auto& x : CapMap) {
+				cout << x.first << "/" << x.second << endl;
+			}
+		}
+
 
 	}
 	system("pause");
 	return 0;
+
+	
 }
