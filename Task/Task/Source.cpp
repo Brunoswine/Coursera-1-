f5574bb58db3109c2973c60719406b52b11d1e5b
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-25 21:39:36 +0300

    Anagrams

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -1,79 +1,36 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 using namespace std;
 
-//Список дел,наконец сделал ааааа Неделя 2
+//Анаграммы, Неделя 2
 
-
+map<char, int> BuildCharCounters(const string& s1){  // функция которая создает словарь символов(сколько раз символ встречается) для строки 
+	map<char, int> m;
+	for (char word : s1) {  //Обращение к несушествующему ключу создаст его со значением 0;
+		m[word]++;
+	}
+	return m;
+}
 
 int main() {
 	int N;
-	bool f;
-	int sum = 0;
 	cin >> N;
-	int command;
-	int k;
-	int current_month = 0;
-	string s;
-	string strCommand;
-	vector<int> day_in_month = {32,29,32,31,32,31,32,32,31,32,31,32};
-	vector<vector<string>> LIST(32);
+	string s1, s2;
+	map<char, int> count1;
+	map<char, int> count2;
+
 	for (int i = 0; i < N; i++) {
-		f = false;
-		cin >> strCommand;
-		if (strCommand == "ADD") {
-			cin >> k >> s;
-			LIST[k].push_back(s);
-		}
-		if (strCommand == "DUMP") {
-			cin >> k;
-			cout << LIST[k].size()<<" ";
-			for (int i = 0; i < LIST[k].size();i++) {
-				cout << LIST[k][i]<<" ";
-			}
-			cout << endl;
-		}
-
-		if (strCommand == "Quit") {
-			f = true;
-		}
-		if (f) break;
-		
-		
-		if (strCommand == "NEXT") {
-			current_month++;
-			int day_in_this;
-			int day_in_pred;
-			if (current_month > 11) { current_month = 0; 
-			day_in_pred = 32;
-			}
-			else { day_in_pred = day_in_month[current_month - 1]; }
-			 day_in_this = day_in_month[current_month];
-		 
-			vector<vector<string>> tmp = LIST;
-			LIST.resize(day_in_this);
-			if (day_in_this < day_in_pred) { //если в этом месяце дней меньше, чем в предыдущем
-				int raznica = day_in_pred - day_in_this;
-				for (int i = 0; i < raznica; i++) {
-					/*vector<string> temp; // создадим одинарный вектор того дня, что нужно запушить в последний день и заполним его словами
-					for (int j = 0; j < tmp[day_in_pred - i-1].size(); j++) {
-						temp.push_back(tmp[day_in_pred - i-1][j]);
-					}*/
-					for (string c : tmp[day_in_pred - i - 1]) { // 
-						LIST[day_in_this-1].push_back(c);
-					}
-				}
-			} 
-
-		}
+		cin >> s1 >> s2;
+		count1 = BuildCharCounters(s1);
+		count2 = BuildCharCounters(s2);
+		if (count1 == count2) {
+			cout << "YES" << endl;
+		} else 
+			cout << "NO" << endl;
 
 	}
-	
-	
-
-
-
-	//system("pause");
+	system("pause");
 	return 0;
 }
