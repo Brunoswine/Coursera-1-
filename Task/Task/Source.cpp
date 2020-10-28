4f0603c45608970ccb4defc4f2b8bff43927b93e
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-29 01:58:36 +0300

    Bus stops

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -16,6 +16,17 @@ bool countryExists(const map<string, string>& m, const string& country) {  //Tru
 	return f;
 }
 
+vector<string> busesSearch(const map<string, vector<string>>& m, const string& stop){
+	vector<string> result;
+	for (const auto& x : m) {
+		for (const string& st : x.second) {
+			if (st == stop) {
+				result.push_back(x.first);
+			}
+		}
+	}
+	return result;
+}
 //Вместо countryExists можно использовать CapMap.count(country) == 0 / CapMap.count(country) == 1
 
 
@@ -27,73 +38,89 @@ int main() {
 	string command,country,new_capital;
 	bool f = false;
 	map<string, string> CapMap;
+	string bus;
+	map<string, vector<string>> route;
+
 	for (int i = 0; i < N; i++) {
-		f = false;								//Сброс флага 
 		cin >> command;
-		if (command == "CHANGE_CAPITAL") {
-			cin >> country;
-			cin >> new_capital;
 
-			f = countryExists(CapMap,country);// если такая страна есть, то нужно поменять, а не добавить. хотя это одно и тоже, хех
-	
+		if (command == "NEW_BUS") {
+			int stop_count;
+			cin >> bus;
+			cin >> stop_count;
+			string stop1;
+			vector<string> stops;
+			for (int j = 0; j < stop_count; j++) {
+				cin >> stop1;
+				stops.push_back(stop1);
+			}
+			route[bus] = stops;
+		}
+		if (command == "BUSES_FOR_STOP") { //Реализуем поиск, в каждом элементе словаря route (номер маршрута), пройдемся по каждой остановке, и, если эта остановка совпадает с запрашиваемой, вывести номер маршрута.
+			bool flag = false;             // x.first - номер автобуса, x.second - вектор с остановками
+			string stop;
+			cin >> stop;
+			for (const auto & x : route) {
+				for (string st : x.second) {
+					if (st == stop) { 
+						cout << x.first<<" "; 
+					flag = true; }
+				}
+			}
+			if (flag) {
+				cout << endl;
+			}
+			if (!flag) {
+				cout << "No stop" << endl;
+			}
+		}
 
-			if (f) {
-				const string& old_capital = CapMap[country]; //опять же, не копирование, а создание ссылки на столицу, хотя это почти не ускорит.
-				if (new_capital == old_capital) { // если нечего менять
-					cout << "Country " << country << " hasn't changed its capital" << endl;
+		if (command == "STOPS_FOR_BUS") {
+			bool fl = false;
+			string bus;
+			cin >> bus;
+			if (route.count(bus) == 0) {
+				cout << "No bus" << endl;
+				fl = true;
+			}
+			if (fl) continue;
+
+
+			for (string &stop : route[bus]) { // Итерирумеся по вектору остановок
+				vector<string> buses = busesSearch(route, stop);  //создаем вектор пересечений остановки с другими маршрутами(пересадки)
+				buses.erase(buses.begin()); // удаление исходного автобуса
+				cout <<"Stop "<< stop<<": ";
+				if (buses.size() == 0) {
+					cout << "no interchange"<<endl;
 				}
 				else {
-					cout << "Country " << country << " has changed its capital from " << old_capital << " to " << new_capital << endl;
-					CapMap[country] = new_capital;
+					for (string& buss : buses) {
+						cout << buss << " ";
+					}
+					cout << endl;
 				}
-
-			}
-
-			if (!f) {
-				CapMap[country] = new_capital;
-				cout << "Introduce new country " << country << " with capital " << new_capital << endl;
-			}
-
-		}
-
-		if (command == "RENAME") {
-			string old_country_name, new_country_name;
-			cin >> old_country_name >> new_country_name;
-			if ((old_country_name == new_country_name) || (!countryExists(CapMap,old_country_name)) || (countryExists(CapMap, new_country_name))) { // Incorrect rename, skip, если новое название страны совпадает со старым, страна old_country_name не существует или страна new_country_name уже существует
-				cout << "Incorrect rename, skip" << endl;
-			}
-			else{
-				cout << "Country "<< old_country_name<<" with capital " << CapMap[old_country_name]<<" has been renamed to "<< new_country_name << endl;
-				string temp = CapMap[old_country_name];
-				CapMap.erase(old_country_name);
-				CapMap[new_country_name] = temp;
-			}
-
-		}
-		if (command == "ABOUT") {
-			cin >> country;
-			if (!countryExists(CapMap, country)) {
-				cout << "Country " << country << " doesn't exist" << endl;
-			}
-			else { 
-				cout << "Country " << country << " has capital " << CapMap[country] << endl;
 			}
 		}
+		if (command == "ALL_BUSES") {
+			bool fl = false;
+			if (route.count(bus) == 0) {
+				cout << "No buses" << endl;
+				fl = true;
+			}
+			if (fl) continue;
+
+			for (auto& x : route) {
+				cout << "Bus " << x.first << ": ";
+				for (string st : x.second) {
+					cout << st << " ";
+				}
+				cout << endl;
+			}
+		}
+
 		
-
-		if (command == "DUMP") {
-			if (CapMap.size() == 0) {
-				cout<<"There are no countries in the world"<<endl;
-			}
-			for (const auto& x : CapMap) {
-				cout << x.first << "/" << x.second << endl;
-			}
-		}
-
-
+		
 	}
 	system("pause");
 	return 0;
-
-	
 }
