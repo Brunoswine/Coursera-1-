f6d00409aa59d9eef4218cbb178190b9c16540dc
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-29 17:03:00 +0300

    Unique strings

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -1,45 +1,21 @@
 #include <iostream>
+#include <set>
 #include <string>
-#include <vector>
-#include <map>
+
 using namespace std;
 
-//Автобусные остановки - 2, Неделя 2
+//Количество уникальных строк, Неделя 2
 
 int main() {
-	bool flag;
-	int N, Q;
+	int N;
 	cin >> N;
-	int num=1;
-	int temp = 0;
-	vector<string> stops;
-	map<int, vector<string>> route;
-
+	string st;
+	set<string> str;
 	for (int i = 0; i < N; i++) {
-		flag = false;
-		cin >> Q;
-		stops.clear();
-		for (int j = 0; j < Q; j++) {
-			string stop1;
-			cin >> stop1;
-			stops.push_back(stop1);
-		}
-
-		for (const auto & x : route) {  // Проверяем, существует ли такой маршрут  if (buses.count(stops) == 0)
-			if (stops == x.second) {
-				flag = true;
-				temp = x.first;
-			}
-		}
-		if (!flag) {
-			route[num] = stops;
-			cout << "New bus " << num << endl;
-			num++;
-		}
-		else {
-			cout << "Already exists for " << temp<<endl;
-		}
+		cin >> st;
+		str.insert(st);
 	}
+	cout << str.size();
 	system("pause");
 	return 0;
 }
