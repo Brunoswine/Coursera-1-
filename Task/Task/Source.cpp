fe6358e7758a81aad891a3d71f949c0bbef12b40
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-29 16:32:38 +0300

    Bus stop - 2(routes)

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -4,138 +4,41 @@
 #include <map>
 using namespace std;
 
-//—правочник столиц, Ќедел€ 2
-
-
-bool countryExists(const map<string, string>& m, const string& country) {  //True, если така€ страна существует, ссылки, чтобы не копировать
-	bool f = false;
-	for (auto &x : m) {
-		if (x.first == country)
-			f = true;
-	}
-	return f;
-}
-
-vector<string> busesSearch(map<string, vector<string>>& m, const string& stop, vector<string>& orderOfCreation){
-	vector<string> result;
-	for ( string& str : orderOfCreation) {
-		for ( string& st : m[str]) {
-			if (st == stop) {
-				result.push_back(str);
-			}
-		}
-	}
-	return result;
-}
-//¬место countryExists можно использовать CapMap.count(country) == 0 / CapMap.count(country) == 1
-
+//јвтобусные остановки - 2, Ќедел€ 2
 
 int main() {
-	int N;
+	bool flag;
+	int N, Q;
 	cin >> N;
-	string command;
-	string bus;
-	vector <string> orderOfCreation; // вектор пор€дка создани€ маршрутов
-	map<string, vector<string>> route;
+	int num=1;
+	int temp = 0;
+	vector<string> stops;
+	map<int, vector<string>> route;
 
 	for (int i = 0; i < N; i++) {
-		cin >> command;
-
-		if (command == "NEW_BUS") {
-			int stop_count;
-			cin >> bus;
-			cin >> stop_count;
-			orderOfCreation.push_back(bus);
+		flag = false;
+		cin >> Q;
+		stops.clear();
+		for (int j = 0; j < Q; j++) {
 			string stop1;
-			vector<string> stops;
-			for (int j = 0; j < stop_count; j++) {
-				cin >> stop1;
-				stops.push_back(stop1);
-			}
-			route[bus] = stops;
-		}
-		if (command == "BUSES_FOR_STOP") { //–еализуем поиск, в каждом элементе словар€ route (номер маршрута), пройдемс€ по каждой остановке, и, если эта остановка совпадает с запрашиваемой, вывести номер маршрута.
-			bool flag = false;             // x.first - номер автобуса, x.second - вектор с остановками
-			string stop;
-			cin >> stop;
-
-			for (string& str : orderOfCreation) {
-				for (string& st : route[str]) {
-					if (st == stop) {
-						cout << str << " ";
-						flag = true;
-					}
-				}
-			}
-			/*
-			for (const auto & x : route) {
-				for (string st : x.second) {
-					if (st == stop) { 
-						cout << x.first<<" "; 
-					flag = true; }
-				}
-			}
-			*/
-			if (flag) {
-				cout << endl;
-			}
-			if (!flag) {
-				cout << "No stop" << endl;
-			}
+			cin >> stop1;
+			stops.push_back(stop1);
 		}
 
-		if (command == "STOPS_FOR_BUS") {
-			bool fl = false;
-			string bus;
-			cin >> bus;
-			if (route.count(bus) == 0) {
-				cout << "No bus" << endl;
-				fl = true;
-			}
-			if (fl) continue;
-
-
-			for (string &stop : route[bus]) { // »терирумес€ по вектору остановок
-				vector<string> buses = busesSearch(route, stop, orderOfCreation);  //создаем вектор пересечений остановки с другими маршрутами(пересадки)
-				int index = 0;
-				for (int i = 0; i < buses.size(); i++) {  // удаление исходного автобуса
-					if (buses[i] == bus){
-						index = i;
-						break;
-					}
-				}
-				buses.erase(buses.begin()+index);
-				cout <<"Stop "<< stop<<": ";
-				if (buses.size() == 0) {
-					cout << "no interchange"<<endl;
-				}
-				else {
-					for (string& buss : buses) {
-						cout << buss << " ";
-					}
-					cout << endl;
-				}
+		for (const auto & x : route) {  // ѕровер€ем, существует ли такой маршрут  if (buses.count(stops) == 0)
+			if (stops == x.second) {
+				flag = true;
+				temp = x.first;
 			}
 		}
-		if (command == "ALL_BUSES") {
-			bool fl = false;
-			if (route.count(bus) == 0) {
-				cout << "No buses" << endl;
-				fl = true;
-			}
-			if (fl) continue;
-
-			for (auto& x : route) {
-				cout << "Bus " << x.first << ": ";
-				for (string st : x.second) {
-					cout << st << " ";
-				}
-				cout << endl;
-			}
+		if (!flag) {
+			route[num] = stops;
+			cout << "New bus " << num << endl;
+			num++;
 		}
-
-		
-		
+		else {
+			cout << "Already exists for " << temp<<endl;
+		}
 	}
 	system("pause");
 	return 0;
