02d295f83df5b721b6168c7e5bded37880e3f517
Ivan Yakushev 850503 <ivan.yakushev.2001@gmail.com>
2020-10-29 17:12:17 +0300

    BuildMapValuesSet

--- Task/Task/Source.cpp
+++ Task/Task/Source.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <set>
 #include <string>
+#include <map>
 
 using namespace std;
 
+set<string> BuildMapValuesSet(const map <int, string>& m) {
+	set<string> s;
+	for (const auto& x : m) {
+		s.insert(x.second);
+	}
+	return s;
+}
 //Количество уникальных строк, Неделя 2
 
 int main() {
-	int N;
-	cin >> N;
-	string st;
-	set<string> str;
-	for (int i = 0; i < N; i++) {
-		cin >> st;
-		str.insert(st);
+	set<string> values = BuildMapValuesSet({
+	{1, "odd"},
+	{2, "even"},
+	{3, "odd"},
+	{4, "even"},
+	{5, "odd"}
+		});
+
+	for (const string& value : values) {
+		cout << value << endl;
 	}
-	cout << str.size();
+
 	system("pause");
 	return 0;
 }
